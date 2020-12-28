4bdd9d42a4628f37967de696d1f3b62d16fdfce0
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-12-28 22:18:59 +0400

    CRUD3

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         val viewModel: PostViewModel by viewModels()
         val adapter = PostAdapter(object : OnInteractionListener {
             override fun onEdit(post: Post) {
+                binding.group.visibility = View.VISIBLE
                 viewModel.edit(post)
             }
 
